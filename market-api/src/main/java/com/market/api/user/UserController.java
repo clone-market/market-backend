ac76f7bc171703b1499c240dbcdb78d732f4ed27
gunkims <45007556+kanpann@users.noreply.github.com>
2021-06-12 21:38:04 +0900

    컨트롤러 작성

---
+++ market-api/src/main/java/com/market/api/user/UserController.java
@@ -0,0 +1,9 @@
+package com.market.api.user;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v2/user")
+public class UserController {
+}
